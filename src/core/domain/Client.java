174e72614f43665478893dff6158eefef3db9621
root <vinilemes@live.com>
2018-02-01 16:56:11 -0200

    Build Client Class

---
+++ src/core/domain/Client.java
@@ -0,0 +1,16 @@
+package core.domain;
+
+
+import java.util.List;
+
+public class Client extends People{
+	
+	private int ranking;	
+	private List<Card> cards;
+	private List<Telephone> telephone;
+	private List<Address> address;
+	private List<ExchangeCoupon> exchangecoupon;
+	
+		
+
+}
